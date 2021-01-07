dbd900ed8fb1f60a374d8ad4fc6d25a580698aa7
Arthur Schang <arthur.schang@parsons.com>
2021-01-06 21:21:13 -0500

    Updated scripts to download images to use wget as opposed to aws cli.

--- data/urban/download_alpha_1_ugv_uav.sh
+++ data/urban/download_alpha_1_ugv_uav.sh
@@ -1,3 +1,3 @@
 #/bin/bash
-aws s3 cp s3://subt-data/SubT_Urban_Ckt/a_lvl_1.bag a_lvl_1.bag
-aws s3 cp s3://subt-data/SubT_Urban_Ckt/a_lvl_1_uav.bag a_lvl_1_uav.bag
+wget http://subt-data.s3.amazonaws.com/SubT_Urban_Ckt/a_lvl_1.bag
+wget http://subt-data.s3.amazonaws.com/SubT_Urban_Ckt/a_lvl_1_uav.bag
